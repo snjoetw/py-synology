8f0b077a8e7751c7dd775290ade6672959246fad
Joe Lu <jlu9@apple.com>
2017-10-08 02:22:31 -0700

    Update version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='py-synology',
-    version='0.1.1',
+    version='0.1.2',
     packages=['synology'],
     url='https://github.com/snjoetw/py-synology',
     license='MIT',
