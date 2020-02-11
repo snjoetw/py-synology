eaf7aab9b5963bd529b3f805ffd9db7ad19b3e6d
Joe Lu <snjoetw@gmail.com>
2020-02-10 20:18:08 -0800

    - bumped version to 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='py-synology',
-    version='0.3.0',
+    version='0.4.0',
     packages=['synology'],
     url='https://github.com/snjoetw/py-synology',
     license='MIT',
